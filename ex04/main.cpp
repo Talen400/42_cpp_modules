d5d9d5cbab00b65d5222530929dce726a191d59a
Talen400 <talengithub0@gmail.com>
2026-03-21 20:23:23 -0300

    feat: adding ifstream and ofstream

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	std::cout << "hello, world";
+	if (argc != 4)
+		return (1);
+	
+	std::string filename = argv[1];
+	std::string s1 = argv[2];
+	std::string s2 = argv[3];
+
+	std::ifstream istrm(filename.c_str());
+	
+	if (!istrm.is_open())
+	{
+		std::cerr << "Error infile \n";
+		return (1);
+	}
+
+	std::string outFile = filename + ".replace";
+	std::ofstream ostrm(outFile.c_str());
+	
+	if (!ostrm.is_open())
+	{
+		std::cerr << "Error Outfile \n";
+		return (1);
+	}
+	/* range (7)
+	 *
+	 * template <class InputIterator>  
+	 * 			string  (InputIterator first, InputIterator last);
+	 *
+	 * Iterator intern in ifstream:
+	 * cplusplus.com/reference/iterator/istreambuf_iterator/
+	 *
+	 * std::istreambuf_iterator<char>(infile) => begin of string
+	 * std::istreambuf_iterator<char>() => end of string
+	 */
+
+	std::string content(); 
+	return (0);
 }
+
+
+
+/*
+ * argv[2] => filename
+ * filename => argv[3]
+ *
+ */
